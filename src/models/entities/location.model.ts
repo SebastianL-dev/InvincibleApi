1865b123c4b9e3abd9e8b788c42320d2e5066c39
Sebastián Lozano <sebastianl2.dev@gmail.com>
2025-04-17 13:08:05 -0500

    chore(seed): Update seed files 🌱

--- src/models/entities/location.model.ts
+++ src/models/entities/location.model.ts
@@ -6,14 +6,9 @@ const locationSchema = new mongoose.Schema(
     id: { type: Number, required: true, trim: true, unique: true },
     name: { type: String, required: true, trim: true },
     type: { type: String, required: true, trim: true },
-    inhabitants: {
-      type: mongoose.SchemaTypes.ObjectId,
-      ref: "Species",
-      required: true,
-    },
     images: imagesType,
   },
-  { timestamps: true }
+  { timestamps: true, versionKey: false }
 );
 
 export default mongoose.model("Location", locationSchema);
