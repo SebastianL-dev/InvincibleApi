8f9a2afb906a1b93a9d9035a4fa0c604f177a9d0
Sebastián Lozano <sebastianl2.dev@gmail.com>
2025-12-22 22:39:59 -0500

    fix: Remove redis zod validation 🐛

--- src/zod/env.scheme.ts
+++ src/zod/env.scheme.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const envScheme = z.object({
   MONGO_URI: z.string().min(5, "Invalid MongoDB URI"),
-  REDIS_URL: z.string().min(5, "Invalid Redis URL"),
+  // REDIS_URL: z.string().min(5, "Invalid Redis URL"),
 });
 
 export default envScheme;
